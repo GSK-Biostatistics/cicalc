3b60eae680636742ca5a2c94e0434f05faa0c836
statasarus <christinaefillmore@gmail.com>
2025-05-09 08:13:04 +0100

    Add expand function

---
+++ R/utils.R
@@ -0,0 +1,45 @@
+#' Expand Count Data into Binary Vectors
+#'
+#' Converts count data (number of successes and total sample size) into a binary vector
+#' of TRUE/FALSE values. This is useful for converting summary statistics back into
+#' raw data format for analysis functions that require individual-level data.
+#'
+#' @param x Integer (or vector of integers) representing the number of successes.
+#' @param n Integer (or vector of integers) representing the total number of participants or trials.
+#'
+#' @return A logical vector where TRUE represents a success and FALSE represents a failure.
+#'   The length of the vector equals the sum of all sample sizes.
+#'
+#' @details
+#' For each pair of values in `x` and `n`, the function creates a vector with `x` TRUE values
+#' followed by `n-x` FALSE values. If multiple pairs are provided, the resulting vectors are
+#' concatenated in order.
+#'
+#' @examples
+#' # Convert 4 successes out of 13 trials to binary data
+#' expand(4, 13)
+#'
+#' # Convert multiple groups of data
+#' # Group 1: 9 successes out of 10
+#' # Group 2: 3 successes out of 10
+#' expand(c(9, 3), c(10, 10))
+#'
+#' @export
+expand <- function(x, n){
+
+  # check inputs ---------------------------------------------------------------
+  check_integerish(x)
+  check_integerish(n)
+  check_range(x, range = c(0, Inf), include_bounds = c(TRUE, FALSE))
+  check_range(n, range = c(0, Inf), include_bounds = c(TRUE, FALSE))
+  check_identical_length(x, n)
+  if(any(x > n)){
+    cli::cli_abort("{.arg x} must be smaller than {.arg n}")
+  }
+
+  purrr::map2(x, n, \(x1, n1){
+    c(rep(TRUE, times = x1), rep(FALSE, times = n1-x1))
+  }
+             ) |>
+    purrr::reduce(c)
+}
